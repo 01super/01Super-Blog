cc9c6b3f9565fefd2227e9e32dca5330f703e488
“01super” <fk1024@outlook.com>
2020-01-03 21:23:17 +0800

    merge

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,11 +4,7 @@ module.exports = function(app) {
   app.use(
     proxy('/api', 
       {
-<<<<<<< HEAD
         "target": "http://o1super.cn",
-=======
-        "target": "http://localhost:8090",
->>>>>>> f2f5c883bbc2948929a99aee02a2d76ace9a93bc
         "changeOrigin": true,
       }
     )
